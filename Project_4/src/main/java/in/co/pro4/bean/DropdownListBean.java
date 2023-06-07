b52aff8c078b6c73d6f91c60cae6a8b7c4b37171
anand mishra <Dushyanttiwari04>
2023-06-07 15:22:45 +0530

    ORS

---
+++ Project_4/src/main/java/in/co/pro4/bean/DropdownListBean.java
@@ -0,0 +1,12 @@
+package in.co.pro4.bean;
+
+/**
+ * @author Dushyant Tiwari
+ *
+ */
+public interface DropdownListBean {
+	
+	public String getKey();
+	
+	public String getValue();
+}
---
+++ Project_4/src/main/java/in/co/pro4/exception/DuplicateRecordException.java
@@ -0,0 +1,14 @@
+package in.co.pro4.exception;
+
+/**
+ * DuplicateRecordException thrown when a duplicate record occurred.
+ * 
+ * @author Dushyant Tiwari
+ *
+ */
+public class DuplicateRecordException extends Exception {
+
+	public DuplicateRecordException(String msg) {
+		super(msg);
+	}
+}
